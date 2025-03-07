b8f404871cf8c6b59051ba5d6b3c40a5ff5007e5
Recep Topaktaş <97768839+receptopaktas0@users.noreply.github.com>
2025-03-07 03:43:57 +0300

    Update TKGMServiceAbstract.php

--- src/TKGMServiceAbstract.php
+++ src/TKGMServiceAbstract.php
@@ -9,6 +9,8 @@ abstract class TKGMServiceAbstract
 {
     protected Client $client;
     protected string $baseUrl = 'https://cbsservis.tkgm.gov.tr/megsiswebapi.v3/api/';
+    // kullanıcılar güncel mahalle_id erişmesi mümkün değil hocam. bunun için bir çözümünüz var mı ?
+    
 
     /**
      * @throws GuzzleException
